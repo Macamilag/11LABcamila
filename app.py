713f2044ffd14973ee3e50543865f2e2cdbdcd5e
Macamilag <150455894+Macamilag@users.noreply.github.com>
2023-11-10 10:03:00 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from PIL import Image
+
+st.title ("Mi primera app")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend.")
